31b20abc568a2315ed165bf9af1067f87dcfd006
Pedro Gonçalves <pedrosinho577@gmail.com>
2026-01-20 22:49:28 -0300

    Student average challenge

--- StudentAverage.java
+++ StudentAverage.java
@@ -1,24 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class StudentAverage {
     public static void main(String[] args) {
     Scanner n = new Scanner(System.in);
-
-    System.out.print("How many two-month periods will there be: ");
-    int tmperiods = n.nextInt();
-    n.nextLine(); //I added this line cause the Scanner glitched
+    ArrayList <Double> grades = new ArrayList <>();
     
     System.out.print("Enter the student's name: ");
     String name = n.nextLine();
 
-    int loop = 0;
-    while (loop < tmperiods) {
-    System.out.print("Enter the Student grade: ");    
-    double grade = n.nextDouble();
-    loop ++;
+    System.out.print("How many grades: ");
+    int totalPeriods = n.nextInt();
+
+
+    for(int i = 0; i < totalPeriods; i++){
+    System.out.print("Enter grade "+(i + 1)+ ": ");
+    grades.add(n.nextDouble());
     }
     
-     
-
+    int counter = 0;
+    double sum  = 0;
+    while (counter < grades.size()) {
+        sum += grades.get(counter);
+        counter ++;
+    }
     
+    double average = sum / grades.size();
+
+    if (average >= 7) {
+        System.out.println(name+" approved!");
+    }
+    else if(average >= 5 && average <= 6.9){
+        System.out.println(name+" recovery");
+    }
+    else{
+        System.out.println(name+" failed");
+    }
     }
 }
