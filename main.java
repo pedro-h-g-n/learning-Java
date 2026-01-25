7f9bd807092ef59b4ec8e796ad62668a2bbe48b4
Pedro Gonçalves <pedrosinho577@gmail.com>
2026-01-25 00:55:37 -0300

    twoSum challenge

--- main.java
+++ main.java
@@ -1,20 +1,21 @@
+import java.util.Arrays;
 public class main{
     public static void main(String[] args){
-        Solution sol = new Solution();
-
         int[] nums = {2, 7, 11, 15};
         int target = 18; //example target / nums
 
-        int[] resultado = sol.twoSum(nums, target);
+       int[] resultado = Solution.twoSum(nums, target);
+
+    
     }
 }
 
 
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
 
         for (int i = 0; i < nums.length; i++){
-            System.out.println("I'm loking at the number"+ nums[i]);
+           
             
         for (int j = i + 1; j < nums.length; j++){
             
