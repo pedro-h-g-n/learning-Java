840a7c1b8aecb0c62005f0bb95e419a0955f32c8
Pedro Gonçalves <pedrosinho577@gmail.com>
2026-01-25 02:16:36 -0300

    twoSum challenge from LeetCode
    The first LeetCode challenge!

--- main.java
+++ main.java
@@ -2,18 +2,16 @@ import java.util.Arrays;
 public class main{
     public static void main(String[] args){
         int[] nums = {2, 7, 11, 15};
-        int target = 18; //example target / nums
+        int target = 26; //example target / nums
 
-       int[] resultado = Solution.twoSum(nums, target);
-
+       int[] resultado = Solution.twoSum(nums, target); 
     
     }
 }
 
 
 class Solution {
-    public static int[] twoSum(int[] nums, int target) {
-
+    public static int[] twoSum(int[] nums, int target) { 
         for (int i = 0; i < nums.length; i++){
            
             
@@ -21,11 +19,14 @@ class Solution {
             
 
             if(nums[i] + nums[j] == target){
+                System.out.println("The sym of "+nums[i]+" and "+nums[i+1]+" is iqual to "+target);
+                
                 return new int[]{i, j};
+
+                
             }
         }
         }
 
         return new int[]{};
-    }
 }
