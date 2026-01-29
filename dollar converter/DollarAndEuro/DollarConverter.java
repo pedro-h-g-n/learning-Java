052540aafd7ac67fedf651b574e2e69d2206e45c
Pedro Gonçalves <pedrosinho577@gmail.com>
2026-01-29 01:34:57 -0300

    add euro conversion

--- dollar converter/DollarAndEuro/DollarConverter.java
+++ dollar converter/DollarAndEuro/DollarConverter.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 public class DollarConverter {
     public static void main(String[] args) {
-        Scanner BRL = new Scanner(System.in);
+        Scanner choiceAndBrl = new Scanner(System.in);
 
+        double EUR = 6.23;//Euro value on the day i wrote this.
         double USD = 5.20;//Dollar value on the day i wrote this.
+        
+        System.out.println("1 - Dollar(USD)");
+        System.out.println("2 - Euro(UER)");
+        System.out.println("choose an option:");
+        int option = choiceAndBrl.nextInt();
 
         System.out.println("Enter the value in Reais:");
-        double valueBRL = BRL.nextDouble();
+        double valueBRL = choiceAndBrl.nextDouble();
 
-        double valueUSD = valueBRL/USD;
+        if (option == 1) {
+            System.out.printf("The value in dollars is:$%.2f%n",(valueBRL/USD));
+        }else if (option == 2) {
+            System.out.printf("The value in euro is:€%.2f%n", valueBRL/ EUR);
+        }else{
+            System.out.println("invalid option!");
+        }
 
-        System.out.printf("The value in dollars is:$%.2f%n", valueUSD);
-
-        BRL.close();
+        choiceAndBrl.close();
     }
     
 }
